7780f256fb11cd64bfc24d9550df787b0e568bc4
ldanlc <daniellc03@yahoo.com.br>
2025-07-15 23:54:31 -0300

    1

---
+++ criando variaveis/algoritmos-em-cpp/test.cpp
@@ -0,0 +1,15 @@
+//Criando variáveis
+
+#include <iostream>
+
+int main() {
+	int numHealths = 5;
+	int Score = 1350;
+	std::cout << "************************\n";
+	std::cout << "Player Health: " << numHealths << "\n";
+	std::cout << "Score: " << Score << "\n";
+	std::cout << "************************\n";
+
+	system("PAUSE");
+	return 0;
+}
