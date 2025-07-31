fc5dd13d3dc0044fa15e5459a7d1acfa3ce532da
ldanlc <daniellc03@yahoo.com.br>
2025-07-30 22:00:36 -0300

    1

---
+++ .caracteres.cpp
@@ -0,0 +1,16 @@
+﻿//Criando vari�veis
+
+#include <iostream>
+
+int main()
+{
+	int numHealths = 5;
+	int Score = 1350;
+	std::cout << "********\n";
+	std::cout << "Player Health: " << numHealths << "\n";
+	std::cout << "Score: " << Score << "\n";
+	std::cout << "********\n";
+
+	system("PAUSE");
+	return 0;
+}
